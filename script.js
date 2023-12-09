a2c6658bfc4acb458ea2482aba975d89767468ff
Ganesh Pathan <118661019+GaneshPathan1015@users.noreply.github.com>
2023-12-09 20:49:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+let val1 = parseInt(prompt());
+let val2 = parseInt(prompt());
+ 
+
+fun(val1,val2);
+function fun(val1,val2) {
+	let ans = val1*2;
+	ans+=val2;
+	alert(`The final result is: ${ans}`);
+}
